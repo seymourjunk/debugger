7b2833390e7a72b5a465916cd91cdb9f7fab5967
seymourjunk <gololobova.galya@gmail.com>
2022-11-01 21:40:48 +0300

    init commit

---
+++ include/Debugger.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <debugapi.h>
+
+namespace dbg {
+	class Debugger
+	{
+	public:
+		Debugger(int _pid)
+		{
+			pid = _pid;
+		}
+		void attach();
+		void run();
+
+	private:
+		int pid;
+	};
+}
---
+++ src/Debugger.cpp
@@ -0,0 +1,36 @@
+﻿#include "Debugger.h"
+
+using namespace dbg;
+
+void Debugger::run()
+{
+	DEBUG_EVENT db_event;
+	while (true){
+		WaitForDebugEvent(&db_event, INFINITY);
+	}
+}
+
+void Debugger::attach()
+{
+	if (DebugActiveProcess(pid)) {
+		run();
+		//std::cout << "123";
+	}
+	else
+	{
+		std::cerr << "Unable to attach to the process with PID: " << pid;
+	}
+}
+
+
+int main()
+{
+	int pid = 0;
+	std::cout << "Write a PID of process to attach: ";
+	std::cin >> pid;
+
+	Debugger win_dbg = Debugger(pid);
+	win_dbg.attach();
+
+	return 0;
+}
