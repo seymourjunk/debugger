9cb7d0b86b25dd6231096a5e61d559d95f38359d
seymourjunk <gololobova.galya@gmail.com>
2022-11-07 14:42:40 +0300

    renamed class members added a new ctor

--- include/Debugger.h
+++ include/Debugger.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <iostream>
+#include <string>
 #include <windows.h>
 #include <debugapi.h>
 
@@ -8,14 +9,22 @@ namespace dbg {
 	class Debugger
 	{
 	public:
-		Debugger(int _pid)
+		Debugger (int pid)
 		{
-			pid = _pid;
+			m_pid = pid;
 		}
+
+		Debugger (std::string _path)
+		{
+			m_program_path = _path;
+		}
+		
 		void attach();
 		void run();
+		void create_process();
 
 	private:
-		int pid;
+		int m_pid = 0;
+		std::string m_program_path;
 	};
 }
--- src/Debugger.cpp
+++ src/Debugger.cpp
@@ -2,23 +2,24 @@
 
 using namespace dbg;
 
-void Debugger::run()
-{
-	DEBUG_EVENT db_event;
-	while (true){
-		WaitForDebugEvent(&db_event, INFINITY);
-	}
-}
-
 void Debugger::attach()
 {
-	if (DebugActiveProcess(pid)) {
-		run();
-		//std::cout << "123";
+	DEBUG_EVENT dbg_event;
+
+	while (true){
+		WaitForDebugEvent(&dbg_event, INFINITY);
+	}
+	//ContinueDebugEvent(dbg_event.dwProcessId, dbg_event.dwThreadId, DBG_CONTINUE);
+}
+
+void Debugger::run()
+{
+	if (DebugActiveProcess(m_pid)) {
+		attach();
 	}
 	else
 	{
-		std::cerr << "Unable to attach to the process with PID: " << pid;
+		std::cerr << "Unable to attach to the process with PID: " << m_pid;
 	}
 }
 
@@ -26,11 +27,31 @@ void Debugger::attach()
 int main()
 {
 	int pid = 0;
+	std::string path = "";
+
+	//TODO: add a method to attach to new process not existed yet
+	// int choice = 0;
+	//std::cout << "Write 1 to attach to existing process" << std::endl;
+	//std::cout << "Write 2 to attach to new process" << std::endl;
+	//std::cin >> choice;
+
+	//if (choice) {
+	//	std::cout << "Write a PID of process to attach: ";
+	//	std::cin >> pid;
+	//	Debugger win_dbg(pid);
+	//	win_dbg.run();
+	//}
+	//else {
+	//	std::cout << "Write a path to create a process and attach: ";
+	//	std::cin >> path;
+	//	Debugger win_dbg(path);
+	//}
+	
 	std::cout << "Write a PID of process to attach: ";
 	std::cin >> pid;
-
-	Debugger win_dbg = Debugger(pid);
-	win_dbg.attach();
+	Debugger win_dbg(pid);
+	win_dbg.run();
+	
 
 	return 0;
 }
